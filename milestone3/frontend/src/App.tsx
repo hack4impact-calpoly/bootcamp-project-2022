5f0be9b0f72a1fd09583ad8ed0390a1ca1840b06
KimLinhVu <klvbubble@gmail.com>
2022-10-31 21:24:09 -0700

    added routing/linking to navbar

--- milestone3/frontend/src/App.tsx
+++ milestone3/frontend/src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import Navbar from './components/navbar';
 import Home from './components/Home';
+import About from './components/About';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className='app'>
+    <BrowserRouter>
       <Navbar />
-      <Home />
-      
-    </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path = "about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- milestone3/frontend/src/components/navbar.tsx
+++ milestone3/frontend/src/components/navbar.tsx
@@ -1,15 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./navbar.css";
 
 export default function Navbar() {
   return (
-    // replace everything in between the <header> & <header /> tags
-    // with your navbar code from your earlier milestones
     <header>
       <h1> Recipe Website </h1>
       <nav>
-        <a>Home</a>
-        <a>About Me</a>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
       </nav>
     </header>
   );
