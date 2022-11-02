490d1d963a87810af8753bcfe3ce38d124e27e47
joieng1 <johncieng1@gmail.com>
2022-11-02 16:14:28 -0700

    feat: created about and home components based on home.html and about.html

---
+++ milestone-3/frontend/src/components/Home.tsx
@@ -0,0 +1,21 @@
+import "./Home.css"
+import RecipeCard from "./RecipeCard";
+import recipeData from "../recipeData.json";
+import Navbar from "./Navbar"
+
+function Home() {
+    return (
+        <div className="App">
+            <Navbar />
+            <main>
+                <h1 className="title">Check out some of our favorites!</h1>
+                {/* <!-- list of recipes --> */}
+                <div className="container-recipes">
+                    {recipeData.map(recipe => <RecipeCard name={recipe.name} image={recipe.image} desc={recipe.description} />)}
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default Home;
