035495a9787315bb738ad957a87017095304b3b0
Ameer Arsala <ameer.arsala03@gmail.com>
2022-11-06 16:07:50 -0800

    Initial milestone 3 commit

---
+++ milestone-3/frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Nav/>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
