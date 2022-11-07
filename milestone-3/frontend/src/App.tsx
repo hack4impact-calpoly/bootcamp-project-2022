e95db64e4ad86f670eafa1573743d40d6e90997d
vilinh <vilinhlvu@gmail.com>
2022-11-06 21:06:48 -0800

    feat: styling

--- milestone-3/frontend/src/App.tsx
+++ milestone-3/frontend/src/App.tsx
@@ -1,12 +1,12 @@
-import Navbar from './components/navbar/navbar';
-import { Home } from './components/home/home';
-import './App.css';
+import Navbar from "./components/navbar/navbar";
+import { Home } from "./components/home/home";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Home />
+        <Navbar />
+        <Home />
     </div>
   );
 }
