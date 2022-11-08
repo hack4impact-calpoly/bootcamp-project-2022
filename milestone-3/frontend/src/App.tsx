74734957446115e87ce048b2dc819d8dee27a6e7
joieng1 <johncieng1@gmail.com>
2022-11-08 15:25:23 -0800

    feat: added react router link to recipeCard.tsx with custom urls. Also updated recipeData.json

--- milestone-3/frontend/src/App.tsx
+++ milestone-3/frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { ChangeEvent, useState } from "react";
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //App components
@@ -12,27 +11,14 @@ import RecipePreview from "./components/RecipePreview";
 
 function App() {
     return (
-        <div className="App">
-            <BrowserRouter>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route
-                        path="/recipes"
-                        element={recipeData.map((recipe) => (
-                            <RecipePreview
-                                name={recipe.name}
-                                image={recipe.image}
-                                desc={recipe.description}
-                                ingredients={recipe.ingredients}
-                                instructions={recipe.instructions[0]}
-                            />
-                        ))}
-                    />
-                </Routes>
-            </BrowserRouter>
-        </div>
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="recipes/:id" element={<RecipePreview />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
--- milestone-3/frontend/src/components/RecipeCard.tsx
+++ milestone-3/frontend/src/components/RecipeCard.tsx
@@ -1,20 +1,36 @@
-import "./RecipeCard.css"
+import "./RecipeCard.css";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 interface Recipe {
     name: string;
     image: string;
     desc: string;
 }
-function RecipeCard(props: Recipe) {
+export default function RecipeCard(props: Recipe) {
     return (
         <div className="card">
             <h2 className="card-title">{props.name}</h2>
             <div className="card-body">
-                <img className="card-img" src={props.image} width="250" height="250"/>
+                <img
+                    className="card-img"
+                    src={props.image}
+                    width="250"
+                    height="250"
+                />
                 <p className="card-disc">{props.desc}</p>
-                <a className="card-btn" href="#">Go</a>
+                <Link
+                    className="card-btn"
+                    to={`/recipes/${props.name
+                        .toLowerCase()
+                        .replace(/[^a-z0-9]/gi, "")}`}
+                >
+                    Go
+                </Link>
             </div>
         </div>
     );
 }
 
-export default RecipeCard
+// export default RecipeCard;
