be83c3282b67f2c882f302849aac28984ad1ebcb
upluke <lukeforadream@gmail.com>
2022-11-08 23:36:28 -0800

    navbar

--- milestone-3/frontend/src/App.tsx
+++ milestone-3/frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import Navbar from './components/navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        Test
+        <Navbar />
     </div>
   );
 }
---
+++ milestone-3/frontend/src/components/navbar.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./navbar.css";
+
+export default function Navbar() {
+  return (
+    // replace everything in between the <header> & <header /> tags
+    // with your navbar code from your earlier milestones
+    <header>
+      <h1> Hu's Chews </h1>
+      <nav className="nav ">
+            <div className="container">
+                <h1 className="logo"><a href="/index.html" className="current">MyKitchen</a> </h1>
+                <ul>
+                    <li><a href="/index.html" className="current">Home</a></li>
+                    <li><a href="/about.html">About</a></li>  
+                </ul>
+            </div>
+        </nav>
+    </header>
+  );
+}
