4c6afea91e023048a8f8c545517168cd62ca3a2a
upluke <lukeforadream@gmail.com>
2022-11-10 23:07:43 -0800

    about page styling

--- milestone-3/frontend/src/components/about.tsx
+++ milestone-3/frontend/src/components/about.tsx
@@ -1,21 +1,19 @@
 import React from "react";
 import "./about.css";
+import aboutImg from '../image/about.jpg'
+
 
 export default function About() {
   return (
     // replace everything in between the <header> & <header /> tags
     // with your navbar code from your earlier milestones
     <div className="About-us">
-       
-            {/* <img src='./img/about.jpg' > */}
-            <h1>Our mission is to help you become a food master.</h1>
-            
-         
-        
-          <p> Please follow us on: </p>
-           <a href="#" className="fa fa-facebook"></a> 
-           <a href="#" className="fa fa-twitter"></a> 
-           <a href="#" className="fa fa-linkedin"></a> 
-          </div>
+       <img src={aboutImg} alt="img" />
+       <h1>Our mission is to help you become a food master.</h1>
+       <p> Please follow us on: </p>
+       <a href="#" className="fa fa-facebook"></a> 
+       <a href="#" className="fa fa-twitter"></a> 
+       <a href="#" className="fa fa-linkedin"></a> 
+    </div>
   );
 }
