cb4f92d8cf17754449480c31a1206a8c394f4b52
upluke <lukeforadream@gmail.com>
2022-11-10 22:58:36 -0800

    recipes shown with photos

--- milestone-3/frontend/src/components/RecipePreview.tsx
+++ milestone-3/frontend/src/components/RecipePreview.tsx
@@ -6,25 +6,32 @@ export default function RecipePreview(recipe:any) {
   return (
 		// replace everything between the <div> & </div> tags
 		// with your code from earlier milestones
-    <div>
-      <h3> {recipe.name}</h3>
-      <div>
-        {/* <img src="../image/sushi.jpg" /> */}
-        <img src={recipe.image} alt="img" />
-        <h3>Description</h3>
-        <p>{recipe.description}</p>
-        <h3>Ingredients</h3>
-        <ul>
-        {recipe.ingredients.map((ingredient:any)=>
-            <li>{ingredient}</li>
-        )}
-        </ul>
-        <h3>Method</h3>
-        <ol>
-        {recipe.instructions.map((instruction:any)=>
-            <li>{instruction}</li>
-        )} 
-        </ol>
+    <div className='recipe_page' >
+      {/* <h3> {recipe.name}</h3> */}
+      <header>
+        <h1> {recipe.name}</h1>
+      </header>
+      <div className="container">
+       
+        <section id='left_section'>
+            <img src={recipe.image} alt="img" />
+        </section>
+        <section id='right_section'>
+            <h3>Description</h3>
+            <p>{recipe.description}</p>
+            <h3>Ingredients</h3>
+            <ul id="description">
+            {recipe.ingredients.map((ingredient:any)=>
+                <li>{ingredient}</li>
+            )}
+            </ul>
+            <h3>Method</h3>
+            <ol>
+            {recipe.instructions.map((instruction:any)=>
+                <li>{instruction}</li>
+            )} 
+            </ol>
+        </section>
       </div>
 	  </div>
   );
