ba04b288ab522100760c0ab638080c81e010e0c5
joieng1 <johncieng1@gmail.com>
2022-11-13 18:07:22 -0800

    feat: Implemented add instruction and ingredients function to recipe pages.

--- milestone-3/frontend/src/components/RecipePage.tsx
+++ milestone-3/frontend/src/components/RecipePage.tsx
@@ -1,6 +1,7 @@
 import "./RecipePage.css";
 import { useParams } from "react-router-dom";
 import recipeData from "../recipeData.json";
+import React, { ChangeEvent, useState } from "react";
 
 interface Recipe {
     link_name: string;
@@ -13,15 +14,20 @@ interface Recipe {
 
 const RecipePage = () => {
     const { id } = useParams();
-    function search(data: Recipe[], name: String | undefined) {
-        for (let j = 0; j < data.length; j++) {
-            if (data[j].link_name === name) {
-                return j;
-            }
-        }
-        return -1;
-    }
-    let i = search(recipeData, id);
+    let i = recipeData.findIndex((data: Recipe) => data.link_name === id);
+
+    const [newIngredient, setNewIngredient] = useState("");
+    // State variable stores ingredient data as initial state
+    const [allIngredients, setAllIngredients] = useState(
+        recipeData[i].ingredients
+    );
+
+    const [newInstruction, setNewInstruction] = useState("");
+    // State variable stores instruction data as initial state
+    const [allInstructions, setAllInstrutctions] = useState(
+        recipeData[i].instructions
+    );
+
     return (
         <main>
             <div className="recipe-container">
@@ -40,24 +46,62 @@ const RecipePage = () => {
                     />
                     <div className="ingredients">
                         <h1 className="title-color">Ingredients</h1>
+                        {/* Renders all ingredients */}
                         <ul className="small-font">
-                            {recipeData[i].ingredients.map(function (
-                                name,
-                                index
-                            ) {
+                            {allIngredients.map(function (name, index) {
                                 return <li key={index}>{name}</li>;
                             })}
                         </ul>
+                        <input
+                            className="input-box"
+                            placeholder="2 cups of spinach"
+                            value={newIngredient} // add newIngredient as the input's value
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                                // this event handler updates the value of newIngredient
+                                setNewIngredient(e.target.value);
+                            }}
+                        />
+                        <button
+                            className="addButton"
+                            onClick={() =>
+                                setAllIngredients([
+                                    ...allIngredients,
+                                    newIngredient,
+                                ])
+                            }
+                        >
+                            Add Ingredient
+                        </button>
                     </div>
                 </div>
             </div>
             <div className="recipe-instruct">
                 <h1 className="instruct-title title-color">Instructions</h1>
                 <ol className="small-font">
-                    {recipeData[i].instructions.map(function (name, index) {
+                    {allInstructions.map(function (name, index) {
                         return <li key={index}>{name}</li>;
                     })}
                 </ol>
+                <input
+                    className="input-box"
+                    placeholder="Mix sugar with salt"
+                    value={newInstruction} // add newInstruction as the input's value
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                        // this event handler updates the value of newInstruction
+                        setNewInstruction(e.target.value);
+                    }}
+                />
+                <button
+                    className="addButton"
+                    onClick={() =>
+                        setAllInstrutctions([
+                            ...allInstructions,
+                            newInstruction,
+                        ])
+                    }
+                >
+                    Add Instruction
+                </button>
             </div>
         </main>
     );
