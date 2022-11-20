14e36b26f8b797105c377b26136f6b1342950b10
KimLinhVu <klvbubble@gmail.com>
2022-11-20 12:44:33 -0800

    set up and connected mongodb

--- milestone3/backend/index.ts
+++ milestone3/backend/index.ts
@@ -1,6 +1,14 @@
 import { Express } from "express";
 const express = require("express"); // 1. includes Express
 const app: Express = express(); // 2. initializes Express
+const mongoose = require("mongoose");
+const connection_url = "mongodb+srv://root:test123@cluster0.5zgrd20.mongodb.net/RecipesDB?retryWrites=true&w=majority"
+
+mongoose.connect(connection_url)
+.then(() => console.log("Successfully connected"))
+.catch(() => console.log("Could not connect"))
+
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
