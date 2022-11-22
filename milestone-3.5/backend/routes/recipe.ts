521eb25c2b8e9b59c92f6d669b5a55c12ef47170
Sam Hudson <sam.hud@icloud.com>
2022-11-21 16:22:08 -0800

    feat: backend for milestone 4

---
+++ milestone-3.5/backend/routes/recipe.ts
@@ -0,0 +1,61 @@
+// backend/routes/recipe.ts
+import Recipe from "../models/recipeSchema";
+import express, { Request, Response, Router } from "express";
+
+const router: Router = express.Router();
+
+// get all recipes
+router.get("/", async (req: Request, res: Response) => {
+  const recipes = await Recipe.find({});
+  res.send(recipes);
+});
+
+// get specific recipe
+router.get("/:name", async (req: Request, res: Response) => {
+  const { name } = req.params;
+  const recipe = await Recipe.findOne({
+    name: name,
+  });
+  res.send(recipe);
+});
+
+router.post("/", async (req: Request, res: Response) => {
+  await Recipe.create(req.body);
+  res.send("Recipe created");
+});
+
+router.put("/:name", async (req: Request, res: Response) => {
+  await Recipe.findOneAndUpdate({ name: req.params.name }, req.body);
+  res.send("Recipe updated");
+});
+
+router.delete("/:name", async (req: Request, res: Response) => {
+  await Recipe.findOneAndDelete({ name: req.params.name });
+  res.send("Recipe deleted");
+});
+
+router.put("/:recipeName/ingredient", async (req: Request, res: Response) => {
+  const { recipeName } = req.params;
+  const { newIngredient } = req.body;
+  const recipe = await Recipe.findOne({ name: recipeName });
+  if (recipe) {
+    recipe.ingredients = [...recipe.ingredients, newIngredient];
+    await recipe.save();
+    res.send("Ingredient added");
+  }
+  res.send("Failed to add ingredient");
+});
+
+router.put("/:recipeName/instruction", async (req: Request, res: Response) => {
+  const { recipeName } = req.params;
+  const { newInstruction } = req.body;
+  const recipe = await Recipe.findOne({ name: recipeName });
+  if (recipe) {
+    recipe.instructions = [...recipe.instructions, newInstruction];
+    await recipe.save();
+    res.send("Instruction added");
+  }
+  res.send("Failed to add instruction");
+});
+
+export default router;
---
+++ milestone-3.5/backend/server.ts
@@ -0,0 +1,21 @@
+import { Express } from "express";
+import mongoose from "mongoose";
+import recipeEndPoints from "./routes/recipe";
+
+const express = require("express"); // 1. includes Express
+const app: Express = express(); // 2. initializes Express
+app.use(express.json()); // Enables JSON parsing
+
+console.log("Backend running");
+
+const url =
+  "mongodb+srv://admin:7cKIwO4vYk2zZeSY@cluster0.0b736wr.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+  .connect(url)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(`Error connecting to DB: ${err}`));
+
+// Routes go here
+app.use("/recipe", recipeEndPoints); // 3. adds the recipe endpoints
+
+app.listen(3001); // 4. runs Express
