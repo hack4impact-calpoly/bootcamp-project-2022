4b5de7d6e87c8d7578a749158e19b83e3605968e
Chloe Lo <sylochloe@gmail.com>
2022-11-24 21:19:12 -0800

    fix: fix the collection name in recipeSchema

--- milestone-3.5/backend/models/recipeSchema.ts
+++ milestone-3.5/backend/models/recipeSchema.ts
@@ -1,39 +1,40 @@
-import { Document, Schema, model } from "mongoose";
+import { Document, Schema, model } from 'mongoose';
 
 export interface Recipe extends Document {
-    name: string;
-    description: string;
-    image: string;
-    ingredients: string[];
-    instructions: string[];
+  name: string;
+  description: string;
+  image: string;
+  ingredients: string[];
+  instructions: string[];
 }
 
-const RecipeSchema = new Schema<Recipe>({
-    name: { 
-        type: String,
-        unique: true,
-        required: true, 
+const RecipeSchema = new Schema<Recipe>(
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: true,
     },
-    description: { 
-        type: String, 
-        required: true,
+    description: {
+      type: String,
+      required: true,
     },
-    image: { 
-        type: String,
-        required: true, 
+    image: {
+      type: String,
+      required: true,
     },
-    ingredients: { 
-        type: [String], 
-        required: true, 
+    ingredients: {
+      type: [String],
+      required: true,
     },
-    instructions: { 
-        type: [String], 
-        required: true, 
+    instructions: {
+      type: [String],
+      required: true,
     },
-    },  
-    { collection: "Recipes" }
+  },
+  { collection: 'Recipes' }
 );
 
-const Recipe = model<Recipe>("Recipe", RecipeSchema);
+const Recipe = model<Recipe>('Recipes', RecipeSchema);
 
 export default Recipe;
