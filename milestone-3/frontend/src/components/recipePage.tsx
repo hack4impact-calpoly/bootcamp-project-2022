00dd296419fe2c6511d1378821112d5c7425eed5
Swayam Chidrawar <schidraw@calpoly.edu>
2022-11-27 02:35:19 -0800

    feat: complete part 1 of milestone 3.5

--- milestone-3/frontend/src/components/recipePage.tsx
+++ milestone-3/frontend/src/components/recipePage.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { ChangeEvent } from 'react'
 import "../recipeData.ts"
 import "./recipePreview.css"
 import { useParams } from 'react-router-dom';
 import { Recipe } from '../recipeData';
 import Navbar from './navbar';
 import recipes from '../recipeData'; 
+import { useState } from 'react';
 
 
 export default function RecipePage() {
     const { name } = useParams(); 
     // Find the recipe corresponding to the selected option.
-    const recipe = recipes.find(i => i.short === name);
-    if (recipe === undefined) { return (<div>Page not found</div>)} 
+    let recipe = recipes.find(i => i.short === name);
+    const [newIngredient, setNewIngredient] = useState('');
+    if (recipe === undefined) { 
+        recipe = recipes[0];
+    } 
+    const [allIngredients, setAllIngredients] = useState(recipe.ingredients);
     return (
     <body>
         <div className='recipe-box'>
@@ -28,6 +33,15 @@ export default function RecipePage() {
                         )
                     )}
                 </ul>
+                <input 
+                placeholder='2 cups of spinach'
+                value = {newIngredient} // add newIngredient as the input's value
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setNewIngredient(e.target.value)}
+                />
+                <button onClick={() => setAllIngredients([...allIngredients, newIngredient])}>
+                    Add Ingredient
+                </button>
             </div>
                 <div>
                     <img className="thumbnail-large" alt={recipe.name} src={recipe.image} />
