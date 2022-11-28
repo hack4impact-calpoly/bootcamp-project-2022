7b56604f80f6fe4c0b2d27555df53046dd02f774
Ryan Agdassi <ragdassi@calpoly.edu>
2022-11-28 10:30:41 -0800

    feat: added input/button states for users to add ingredients or instructions to the respective lists on recipe page

--- milestone3/frontend/src/components/recipePage.tsx
+++ milestone3/frontend/src/components/recipePage.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import {ChangeEvent, useState} from 'react';
 import './recipePage.css';
 import recipes from '../recipeData';
 import { useParams } from 'react-router-dom';
+
 
 interface RecipePage{
     name: string,
@@ -12,33 +14,89 @@ interface RecipePage{
 }
 
 export default function RecipePage() {
+    //prop
     const { id } = useParams();
     const recipe = recipes.find(x => x.name == id)
+    //states - ingredient
+    const [ingredients, setIngredients] = useState(recipe ? recipe.ingredients : [""])
+    const [newIngredient, setNewIngredient] = useState("");
+    //states - instructions
+    const [instructions, setInstructions] = useState(recipe ? recipe.instructions : [""])
+    const [newInstruction, setNewInstruction] = useState("");
+
+    function handleChange(event: ChangeEvent<HTMLInputElement>) {
+        setNewIngredient(event.currentTarget.value);
+    }
+    function handleChange2(event: ChangeEvent<HTMLInputElement>) {
+        setNewInstruction(event.currentTarget.value);
+    }
+
     if (recipe == undefined){
         return <div></div>;
     }
+
     return (
 
         <div className = " backgroundcolor">
             <h1 className = "important text"> {recipe.name} </h1>
-                <br></br>
-            <h2 className = "flex-container4 text"> {recipe.description} </h2>
+            <h4 className = "flex-container4 text"> {recipe.description} </h4>
                 <br></br>
             <img className = "flex-container3 imgscales" src= {recipe.image} />    
-            <h1 className = "important text"> Ingredient List </h1>
-            
+        
+
+            {/* Ingredients */}
+            <h1 className = "important text"> Ingredients </h1>
             
             {/* used map to change make a unordered list of all list elements for ingredients*/}
-
             <ul>
-                {recipe.ingredients.map(ingredient => <li>{ingredient}</li>)}
+                <h3 className = 'text'>
+                {ingredients.map(ingredient => <li>{ingredient}</li>)}
+                </h3>
             </ul>
 
-            {/* used map to change make a unordered list of all list elements for instructions*/}
-            <h1 className = "important text"> Instructions </h1>
-            <ol>
-                {recipe.instructions.map(instruction => <li>{instruction}</li>)}
-            </ol>
+
+            <div className = "flex-container5">
+                <h3 className = 'text'> Add Ingredient:</h3>
+                <div className = "inputbutton">
+                    <input
+                        placeholder="2 cups of spinach"
+                        value={newIngredient} // add newIngredient as the input's value
+                        onChange={handleChange}
+                        
+                    />
+                    <button className = "button" onClick={() => setIngredients([...ingredients, newIngredient])}>
+                        Add Ingredient
+                    </button>
+                </div>
+            </div>
+
+            
+
+            {/* Instructions */}
+
+            
+                {/* used map to change make a unordered list of all list elements for instructions*/}
+                <h1 className = "important text"> Instructions </h1>
+                <ol>
+                    <h3 className = 'text '>
+                    {instructions.map(instruction => <li>{instruction}</li>)}
+                    </h3>
+                </ol>
+            
+            <div className = "flex-container5">
+                <h3 className = 'text'> Add Instructions:</h3>
+                <div className = "inputbutton">
+                    <input
+                        placeholder="Mix well and serve"
+                        value={newInstruction} // add newInstruction as the input's value
+                        onChange={handleChange2}
+                    />
+                    <button className= "button"onClick={() => setInstructions([...instructions, newInstruction])}>
+                        Add Instruction
+                    </button>
+                </div>
+            </div>
+
 
         </div> 
     
@@ -48,4 +106,3 @@ export default function RecipePage() {
 }
 
 
-export{} // to make module?
