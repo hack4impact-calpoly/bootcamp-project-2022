b9456020fd05bbd76d6444070f39e72f42014a48
Kasey Liu <mermaidmagic99@gmail.com>
2022-12-09 16:45:46 -0600

    feat: connected to mongoose

--- milestone-3/backend/index.ts
+++ milestone-3/backend/index.ts
@@ -1,6 +1,13 @@
 import { Express } from "express";
 const express = require("express"); // 1. includes Express
 const app: Express = express(); // 2. initializes Express
+const mongoose = require('mongoose')
+const connection_url = "mongodb+srv://bootcampuser:bootcampdata22@cluster0.epfcxku.mongodb.net/RecipesDB?retryWrites=true&w=majority"
+
+mongoose.connect(connection_url)
+.then(() => console.log('Successfully connected'))
+.catch(() => console.log(`Could not connect`))
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
