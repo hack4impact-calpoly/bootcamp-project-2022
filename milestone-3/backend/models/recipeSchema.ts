bcf8f1e1429baffea744c93bdaf1a8962de5857d
Kasey Liu <mermaidmagic99@gmail.com>
2022-12-09 17:05:26 -0600

    feat: created recipe schema

---
+++ milestone-3/backend/models/recipeSchema.ts
@@ -0,0 +1,22 @@
+import { Document, Schema, model } from "mongoose";
+
+export interface RecipeInterface extends Document {
+  name: string; // type not capitalized
+  description: string;
+  image: string;
+  ingredients: string[];
+  instructions: string[];
+}
+
+const RecipeSchema = new Schema<RecipeInterface>({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  ingredients: {type: [String], required: true },
+  instructions: {type: [String], required: true }
+
+}, {collection: 'Recipes'});
+
+const Recipe = model<RecipeInterface>('Recipes', RecipeSchema);
+
+export default Recipe;
