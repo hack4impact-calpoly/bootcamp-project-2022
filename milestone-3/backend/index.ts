e77e0eeaef33df96a327fda48892a3f3aeeb4cb0
Kasey Liu <mermaidmagic99@gmail.com>
2022-12-10 14:15:47 -0600

    feat: all data from frontend comes from database

--- milestone-3/backend/index.ts
+++ milestone-3/backend/index.ts
@@ -11,6 +11,13 @@ mongoose.connect(connection_url)
 .then(() => console.log('Successfully connected'))
 .catch((error: any) => console.log(`Could not connect due to ${error}`))
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,DELETE,PUT');
+  next();
+});
+
 app.use(express.json());
 app.use("/recipe", recipeRoutes);
 
