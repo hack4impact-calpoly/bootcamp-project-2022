40ee209fa4eaa2de49be67fa1f51b747f6248cab
Dominic Davidson <domdavidson251@gmail.com>
2022-12-10 03:00:41 -0800

    started milestone 3

---
+++ milestone3/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Content will go here
+    </div>
+  );
+}
+
+export default App;
---
+++ milestone3/frontend/src/recipeData.ts
@@ -0,0 +1,15 @@
+export interface Recipe {
+    name: string;
+    description: string;
+    image: string;
+    ingredients: string[];
+    instructions: string[];
+  }
+
+const recipes: Recipe[] = [
+	{recipeObject},
+	{recipeObject},
+	{recipeObject} 
+];
+
+export default recipes;
