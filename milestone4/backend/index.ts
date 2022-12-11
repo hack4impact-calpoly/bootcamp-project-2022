161b46fc518771e2dfd9c442055c77998e85fac9
oli-lane <markus.oliver.lane@gmail.com>
2022-12-11 11:51:23 -0800

    index.ts set up and connecting to db

--- milestone4/backend/index.ts
+++ milestone4/backend/index.ts
@@ -2,7 +2,15 @@ import { Express } from "express";
 
 const express = require("express"); // 1. includes Express
 const app: Express = express(); // 2. initializes Express
-app.use(express.json());
+const mongoose = require('mongoose')
+const connection_url = "mongodb+srv://newUser:newPassword@cluster0.mncyr5p.mongodb.net/RecipesDB?retryWrites=true&w=majority"
+
+mongoose.connect(connection_url)
+.then(() => console.log('Successfully connected'))
+.catch((error: any) => console.error(`Could not connect due to ${error}`))
+
+
+// app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello world!')
