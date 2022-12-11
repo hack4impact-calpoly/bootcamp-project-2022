21d212471cc0587bc30cb2499325e9777ae3f38d
spagdoon0411 <spandan.suthar20042011@gmail.com>
2022-12-11 13:22:14 -0800

    feat: added site icon

--- milestone-3/frontend/src/components/navbar.tsx
+++ milestone-3/frontend/src/components/navbar.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import "./navbar.css"
+import site_icon from "./site-icon.png"
 
 export default function Navbar() 
 {
     return (
         <header className="header">
-            <img src="site-icon.png" className="site-icon" />
+            <img src={site_icon} className="site-icon" />
             <h1><em>that</em> easy</h1>
             <nav>
                 <a href="index.html" className="nav-member">Home</a>
