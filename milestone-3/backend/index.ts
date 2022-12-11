53aa0f59175c89bb033db2509c762955e7bab195
upluke <lukeforadream@gmail.com>
2022-12-11 13:23:07 -0800

    recipe schema

--- milestone-3/backend/index.ts
+++ milestone-3/backend/index.ts
@@ -1,10 +1,17 @@
-import { Express } from "express";
-const express = require("express"); // 1. includes Express
-const app: Express = express(); // 2. initializes Express
+import {Express} from 'express';
+const express = require('express');
+const app: Express = express();
+const mongoose = require('mongoose')
+const connection_url='mongodb+srv://newUser:newPassword@recepecluster.xvth0tb.mongodb.net/RecipsesDB?retryWrites=true&w=majority'
+
+mongoose.connect(connection_url)
+.then(()=>console.log('Successfully connected'))
+.catch((error:any)=>console.error(`Could not coonect due to ${error}`))
+
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello world!')
-});
+app.get('/', (req, res)=>{
+    res.send('Hello world')
+})
 
-app.listen(3001);
+app.listen(3001)
