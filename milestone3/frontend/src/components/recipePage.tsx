5bd5699750e430026e2f5b63bc9b48bf5c8e7868
Dominic Davidson <domdavidson251@gmail.com>
2022-12-12 03:58:59 -0800

    feat(recipePreview): add ingred. and instructions

--- milestone3/frontend/src/components/recipePage.tsx
+++ milestone3/frontend/src/components/recipePage.tsx
@@ -1,31 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.css';
 import recipes from './recipeData';
 import {useParams} from "react-router-dom";
 
 export default function RecipePage() {
     const {id} = useParams();
-    let recipe = recipes.find(recipe => recipe.name === id);
+    const recipe = recipes.find(recipe => recipe.name === id);
+    if (recipe === undefined) {
+        throw new TypeError('Recipe does not exist');
+    }
+
+    const [newIngredient, setNewIngredient] = useState('');
+    const [allIngredients, setAllIngredients] = useState(recipe.ingredients);
+
+    const [newInstruction, setNewInstruction] = useState('');
+    const [allInstructions, setAllInstructions] = useState(recipe.instructions);
+
     return(
         <div className="flex-container">
             <div className="flex-content">
-                <h1>{recipe?.name}</h1>
-                <p>{recipe?.description}</p>
+                <h1>{recipe.name}</h1>
+                <p>{recipe.description}</p>
                 <h2>Ingredients</h2>
                 <ul>
-                    {recipe?.ingredients.map(function(name, index) {
+                    {allIngredients.map(function(name, index) {
                         return <li key={index}>{name}</li>
                     })}
                 </ul>
+                <input
+                    placeholder="A cup of love"
+                    value={newIngredient} // add newIngredient as the input's value
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        // this event handler updates the value of newIngredient
+                        setNewIngredient(e.target.value);
+                    }}
+                />
+                <button onClick={() => setAllIngredients([...allIngredients, newIngredient])}>
+                    Add Ingredient
+                </button>
             </div>
-            <img className="flex-image" src={recipe?.image} alt="food"/>
+            <img className="flex-image" src={recipe.image} alt="food"/>
             <div className="flex-content">
                 <h2>Instructions</h2>
                 <ol>
-                    {recipe?.instructions.map(function(name, index) {
+                    {allInstructions.map(function(name, index) {
                         return <li key={index}>{name}</li>
                     })}
                 </ol>
+                <input
+                    placeholder="Add milk"
+                    value={newInstruction} // add newIngredient as the input's value
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        // this event handler updates the value of newIngredient
+                        setNewInstruction(e.target.value);
+                    }}
+                />
+                <button onClick={() => setAllInstructions([...allInstructions, newInstruction])}>
+                    Add Instruction
+                </button>
             </div>
         </div>
     );
