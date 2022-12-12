a97562f79c5b36aa9455c147efec92790c2806ca
spagdoon0411 <spandan.suthar20042011@gmail.com>
2022-12-12 00:13:49 -0800

    feat: implemented ability to add instructions and ingredients to each recipe

--- milestone-3/frontend/src/components/RecipePage.tsx
+++ milestone-3/frontend/src/components/RecipePage.tsx
@@ -1,7 +1,14 @@
 import { Recipe } from "../recipeData";
 import './RecipePage.css'
+import React, {useState} from "react";
 
 export default function RecipePage(recipe: Recipe ) {
+
+    const [newIngredient, setNewIngredient] = useState('');
+    const [allIngredients, setAllIngredients] = useState(recipe.ingredients);
+
+    const [newInstruction, setNewInstruction] = useState('');
+    const [allInstructions, setAllInstructions] = useState(recipe.instructions);
 
     return(
         <div className="ingredients-recipe-layout">
@@ -10,18 +17,38 @@ export default function RecipePage(recipe: Recipe ) {
                 <h2>Ingredients</h2>
                 <ul className="ingredients-or-recipe-text">
                     {
-                    recipe.ingredients.map((ingredient) => (<li>{ingredient}</li>))
+                    allIngredients.map((ingredient) => (<li>{ingredient}</li>))
                     }
                 </ul>
+                <input
+                    placeholder="3 scoops of disappointment"
+                    value={newIngredient}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        setNewIngredient(e.target.value)
+                    }}
+                />
+                <button onClick={() => setAllIngredients([...allIngredients, newIngredient])}>
+                    Add Ingredient
+                </button>
             </div>
             {/*Flexbox with a list of steps, drawn from recipeData.ts*/}
             <div className="ingredients-or-recipe">
                 <h2>Recipe</h2>
                 <ol className="ingredients-or-recipe-text">
                     {
-                    recipe.instructions.map((step) => (<li>{step}</li>))
+                    allInstructions.map((step) => (<li>{step}</li>))
                     }
                 </ol>
+                <input
+                    placeholder="Place banana in microwave"
+                    value={newInstruction}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        setNewInstruction(e.target.value)
+                    }}
+                />
+                <button onClick={() => setAllInstructions([...allInstructions, newInstruction])}>
+                    Add Step
+                </button>
             </div>
         </div>
     )
