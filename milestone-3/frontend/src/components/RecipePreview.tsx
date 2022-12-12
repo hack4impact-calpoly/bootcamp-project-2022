730e32e43128a00c1e12a90c70f3afcb0d35bed7
spagdoon0411 <spandan.suthar20042011@gmail.com>
2022-12-12 02:00:57 -0800

    fix: forced recipe preview cards to align to the center of the home card

--- milestone-3/frontend/src/components/RecipePreview.tsx
+++ milestone-3/frontend/src/components/RecipePreview.tsx
@@ -5,15 +5,18 @@ import { Recipe } from '../recipeData';
 export default function RecipePreview(recipe: Recipe) {
     return (
         <div>
-            {/*A flexbox containing a recipe icon and name*/}
-            <div className="recipe">
-                <img className="recipe-icons" src={process.env.PUBLIC_URL + recipe.image} alt = {recipe.name} />
-                <Link to={recipe.name}><p className="recipe-text"> {recipe.name}</p></Link>
+            <div className='recipe-wrapper'>
+                {/*A flexbox containing a recipe icon and name*/}
+                <div className="recipe">
+                    <img className="recipe-icons" src={process.env.PUBLIC_URL + recipe.image} alt = {recipe.name} />
+                    <Link to={recipe.name}><p className="recipe-text"> {recipe.name}</p></Link>
+                </div>
+                {/*Text under the flexbox that briefly describes the item*/}
+                <div className='recipe-text'>
+                    { recipe.description }
+                </div>
             </div>
-            {/*Text under the flexbox that briefly describes the item*/}
-            <div className='recipe-text'>
-                { recipe.description }
-            </div>
+            
         </div>
     );
 }
