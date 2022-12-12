19c039f23275c170d8e29eea4e06207894206d92
Dominic Davidson <domdavidson251@gmail.com>
2022-12-12 04:08:12 -0800

    feat: renders recipes from api

--- milestone3/frontend/src/components/Home.tsx
+++ milestone3/frontend/src/components/Home.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from 'react';
 import "./style.css";
 import recipes from './recipeData';
 import {Recipe} from './recipeData';
 import RecipePreview from './recipePreview';
 
 export default function Home() {
+  const [externalRecipes, setExternalRecipes] = useState<Recipe[]>([]);
+  useEffect(() => {
+    fetch("https://bootcamp-milestone-4.onrender.com/recipe")
+      .then((res) => res.json())
+      .then((data) => setExternalRecipes(data));
+  }, []);
+
     return (
       <div>
         <h1>Welcome to Dom's Italian Cuisine!</h1>
         {recipes.map((recipe: Recipe) => (
+            <RecipePreview {...recipe}/>
+        ))}
+        {externalRecipes.map((recipe: Recipe) => (
             <RecipePreview {...recipe}/>
         ))}
       </div>
