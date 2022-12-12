c2cf8256bdb1b3d23da9dfdff176634e38c066ff
upluke <lukeforadream@gmail.com>
2022-12-11 19:36:39 -0800

    update ingredient

--- milestone-3/backend/index.ts
+++ milestone-3/backend/index.ts
@@ -3,13 +3,14 @@ import recipeRouter from './routes/recipe'
 const express = require('express');
 const app: Express = express();
 const mongoose = require('mongoose')
+mongoose.set('strictQuery', true);
 
 const connection_url='mongodb+srv://newUser:newPassword@recepecluster.xvth0tb.mongodb.net/RecipesDB?retryWrites=true&w=majority'
 mongoose.connect(connection_url)
 .then(()=>console.log('Successfully connected'))
 .catch((error:any)=>console.error(`Could not coonect due to ${error}`))
 
-app.use(express.json());
+app.use(express.json()); // include this so express knows express body is in JSON
 
 app.get('/hello', (req, res)=>{
     res.send('Hello world')
--- milestone-3/backend/routes/recipe.ts
+++ milestone-3/backend/routes/recipe.ts
@@ -4,10 +4,31 @@ import Recipe from "../models/recipeSchema"
 const router: Router = express.Router()
 
 // get all recipe 
-router.get("/recipe", async(reqq: Request, res: Response)=>{
+router.get("/recipe", async(req: Request, res: Response)=>{
     const recipes=await Recipe.find({});
     console.log(recipes);
     res.send(recipes);
+})
+
+// get recipe by name
+router.get("/recipe/:name", async(req: Request, res: Response)=>{
+    const recipes=await Recipe.find({name: req.params.name});
+    console.log(recipes);
+    res.send(recipes);
+})
+
+
+// update ingredient in recipe
+router.put("/recipe/:name/ingredient", async(req:Request, res: Response)=>{
+    const recipe = await Recipe.findOne({name:req.params.name})
+    if(recipe){
+        recipe.ingredients=[...recipe.ingredients, req.body.ingredient]
+        await recipe.save()
+        res.send("Added ingredient")
+        return // aovid : Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
+    }
+    res.send("Failed to add ingredient")
+    return  
 })
 
 export default router;
