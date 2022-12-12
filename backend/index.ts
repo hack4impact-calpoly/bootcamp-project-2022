daaa5d7deceda91d329d0497ed25f3da133223c4
TariniS <tareene@gmail.com>
2022-12-12 13:44:58 -0800

    milestone 5

--- backend/index.ts
+++ backend/index.ts
@@ -8,6 +8,16 @@ const app: Express = express(); // 2. initializes Express
 console.log("Backend running");
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS,DELETE,PUT");
+  next();
+  });
+
 const connection_url = "mongodb+srv://newUser:newPassword@cluster0.qjnpuzs.mongodb.net/RecipesDB?retryWrites=true&w=majority"
 mongoose.connect(connection_url)
 .then(() => console.log("sucess"))
--- backend/routes/recipe.ts
+++ backend/routes/recipe.ts
@@ -3,48 +3,61 @@ import Recipe from "../models/recipeSchemas";
 
 const router: Router = express.Router();
 
-
 router.get("/recipe", async (req: Request, res: Response) => {
-    const recipes = await Recipe.find({});
-    res.send(recipes);
-});
-
+  const recipes = await Recipe.find({});
+  console.log(recipes);
+  res.send(recipes);
+})
 
 router.get("/recipe/:name", async (req: Request, res: Response) => {
-    const { name } = req.params;
-    const recipe = await Recipe.findOne({
-      name: name,
-    });
-    res.send(recipe);
-});
-
-
-router.post("/recipe", async (req: Request, res: Response) => {
-  await Recipe.create(req.body);
-  res.send("created recipe from request");
-});
-
-router.put("/recipe/:name/instruction", async (req: Request, res: Response) => {
-    const recipe = await Recipe.findOne({ name: req.params.name });
-    if (recipe) {
-      recipe.instructions = [...recipe.instructions, req.body.instruction];
-      await recipe.save();
-      res.send("added instruction from request");
-    } else {
-      res.send("could not add instruction");
-    }
-  });
+  const recipes = await Recipe.find({name: req.params.name});
+  console.log(recipes);
+  res.send(recipes);
+})
 
 router.put("/recipe/:name/ingredient", async (req: Request, res: Response) => {
-  const recipe = await Recipe.findOne({ name: req.params.name });
+  const recipe = await Recipe.findOne({name: req.params.name});
   if (recipe) {
-    recipe.ingredients = [...recipe.ingredients, req.body.ingredient];
-    await recipe.save();
-    res.send("added ingredient");
-  } else {
-    res.send("could not add ingredient");
+      recipe.ingredients = [...recipe.ingredients,req.body.value];
+      await recipe.save();
+      res.send("Added Ingredient");
   }
-});
+  else {
+  res.send("failed to add ingredient");
+  }
+})
+
+router.put("/recipe/:name/instruction", async (req: Request, res: Response) => {
+  const recipe = await Recipe.findOne({name: req.params.name});
+  if (recipe) {
+      recipe.instructions = [...recipe.instructions,req.body.value];
+      await recipe.save();
+      res.send("Added Instruction");
+  }
+  else {
+  res.send("failed to add instruction");
+  }
+})
+
+router.post("/recipe", async (req: Request, res: Response) => {
+  const { name, description, image, ingredients, instructions } = req.body
+  let recipe = new Recipe({
+      name,
+      description,
+      image,
+      ingredients,
+      instructions
+    })
+ 
+    try {
+      recipe = await recipe.save();
+      res.send(`Recipe named ${name}  added to collection`);
+    } catch(error: any) {
+
+      res.status(500).send(error.message)
+      console.log(`error is ${error.message}`)
+    }
+})
 
 
 
