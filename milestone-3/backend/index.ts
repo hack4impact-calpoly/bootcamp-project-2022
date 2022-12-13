a214d22058618bf86619cea6f896c125f1d3a0b2
spagdoon0411 <spandan.suthar20042011@gmail.com>
2022-12-12 16:30:46 -0800

    feat: connected to recipes database in index.ts

--- milestone-3/backend/index.ts
+++ milestone-3/backend/index.ts
@@ -1,6 +1,13 @@
 import { Express } from "express";
 const express = require("express"); // 1. includes Express
 const app: Express = express(); // 2. initializes Express
+const mongoose = require('mongoose')
+const connection_url = "mongodb+srv://newUser:newPassword@cluster0.iqyoofp.mongodb.net/RecipesDB?retryWrites=true&w=majority"
+
+mongoose.connect(connection_url)
+.then(() => console.log("Successfully connected "))
+.catch((error) => console.error(`Could not connect: ${error}`))
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
