05d06b5827f759ad6cf7451b64181f3bec2b6b96
hanmosh <hannahmoshtag@calpoly.edu>
2022-12-13 02:07:04 -0800

    feat: finished milestone 4

--- milestone-3/my-app/backend/routes/recipeRoutes.ts
+++ milestone-3/my-app/backend/routes/recipeRoutes.ts
@@ -7,7 +7,7 @@ app.get("/", async (req: Request, res: Response) => {
     // get all recipes 
     const recipes = await Recipe.find({});
     res.send(recipes);
-})
+});
 
 app.get("/:name", async (req: Request, res: Response) => {  
     // get specific recipe   
@@ -52,8 +52,37 @@ app.post("/person", async (req: Request, res: Response) => {
       res.status(500).send(error.message)
       console.log(`error is ${error.message}`)
     }
-})
+});
 
+app.put("/:recipeName/ingredient", async (req: Request, res: Response) => {
+    // add ingredient to a recipe
+    const recipeName = req.params.recipeName;
+    const newIngredient = req.body.newIngredient;
+    const recipe = await Recipe.findOne({name: recipeName});
+    
+    if (recipe){
+        recipe.ingredients = [...recipe.ingredients, newIngredient];
+        await recipe.save();
+        res.send(`${newIngredient} added to ${recipeName}`);
+    }else {
+        res.send(`Failed to add ${newIngredient} to ${recipeName}`);
+    }
+});
+
+app.put("/:recipeName/instruction", async (req: Request, res: Response) => {
+    // add step to a recipe
+    const recipeName = req.params.recipeName;
+    const newStep = req.body.newStep;
+    const recipe = await Recipe.findOne({name: recipeName});
+    
+    if (recipe){
+        recipe.instructions = [...recipe.instructions, newStep];
+        await recipe.save();
+        res.send(`${newStep} added to ${recipeName}`);
+    }else {
+        res.send(`Failed to add ${newStep} to ${recipeName}`);
+    }
+});
 
 
 export default app;
