37e4f5e1cfea0056d1c6969c8d3e6029f9514cbc
Rajvir Vyas <rajvirvyas23@gmail.com>
2022-12-13 20:36:29 -0800

    feat: finished all code for Milestone 3.5

---
+++ Milestone 3.5 Submission/Home.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+import './Home.css';
+import { Link} from "react-router-dom";
+import recipes from "./recipeData";
+import RecipePreview from './RecipePreview';
+
+interface Recipe {
+    name: string;
+    description: string;
+    image: string;
+    ingredients: string[];
+    instructions: string[];
+}
+
+function Home() {
+    let [extRecipes, setExtRecipes] = useState<Recipe[]>([]);
+    useEffect(() => {
+        fetch("https://bootcamp-milestone-4.onrender.com/recipe")
+          .then((res) => res.json())
+          .then((data) => setExtRecipes(data));
+    }, []);
+    return (
+        <main>
+        <h1 id="Welcome">Welcome To Rajvir's Ristorante!</h1>
+        {recipes.map((recipe) => (
+            <RecipePreview {...recipe} />))
+        };
+        {extRecipes.map((recipe) => (
+            <RecipePreview {...recipe} external />))
+        };
+        </main>
+    );
+}
+
+export default Home;
