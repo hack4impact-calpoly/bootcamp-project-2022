84629670b2e73c6a57c55abda4839eecc0f06d93
joieng1 <johncieng1@gmail.com>
2022-12-13 22:25:42 -0800

    fix: installed cors module to disable cors. (Solution on notion did not work on PUT requests)

--- milestone-3/backend/index.ts
+++ milestone-3/backend/index.ts
@@ -2,6 +2,8 @@ import { Express } from "express";
 import recipeRoutes from "./routes/recipe";
 const express = require("express"); // 1. includes Express
 const app: Express = express(); // 2. initializes Express
+
+const cors = require("cors");
 app.use(express.json());
 
 const mongoose = require("mongoose");
@@ -16,15 +18,20 @@ mongoose
     );
 
 // to address CORS issue
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS,DELETE,PUT");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS,DELETE,PUT");
+//     next();
+// });
+app.use(
+    cors({
+        origin: "*",
+    })
+);
 
 app.use("/recipe", recipeRoutes);
 
