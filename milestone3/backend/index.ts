bb6bc49033db07b870d0d41c02a6a624c9ca5d30
Ryan Agdassi <ragdassi@calpoly.edu>
2022-12-26 19:06:42 -0800

    feat: added endpoints for getting recipe, adding new recipe, and adding instructions/ingredients

--- milestone3/backend/index.ts
+++ milestone3/backend/index.ts
@@ -1,8 +1,11 @@
 import express, { Express } from "express";
+import recipeRouter from "./routes/recipe"
+
 const app: Express = express(); // 2. initializes Express
+app.use(express.json());
+
 const mongoose = require('mongoose')
 const connection_url = "mongodb+srv://ryan:newPassword@cluster0.hzbf3h2.mongodb.net/RecipesDB?retryWrites=true&w=majority"
-app.use(express.json());
 
 mongoose.connect(connection_url)
 .then(() => console.log("Successful Connection"))
@@ -11,6 +14,8 @@ mongoose.connect(connection_url)
 app.get('/', (req, res) => {
   res.send('Hello world!!')
 });
+
+app.use('/', recipeRouter)
 
 app.listen(3001);
 export default app;
