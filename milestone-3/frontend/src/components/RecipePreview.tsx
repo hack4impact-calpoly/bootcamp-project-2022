f5bc7d066f04e8adc9fdb6af54ceae41716c6e00
lemezze <86095301+lemezze@users.noreply.github.com>
2022-12-30 20:35:42 -0800

    feat: adding external recipe previews

--- milestone-3/frontend/src/components/RecipePreview.tsx
+++ milestone-3/frontend/src/components/RecipePreview.tsx
@@ -1,35 +1,43 @@
-import React from 'react';
-import './recipePreview.css'
-import { Link } from 'react-router-dom';
-import {Recipe} from '../recipeData';
+import React from "react";
+import "./recipePreview.css"
+import { Link } from "react-router-dom";
+import {Recipe} from "../recipeData";
 
 
-/*interface Recipe{
-    name: string;
-    description: string;
-    image: string;
-    ingredients: string[];
-    instructions: string[];
-    altDesc: string
+interface RecipePreviewProps extends Recipe {
+  // The extends keyword means that the props include everything
+  // from Recipe, as well as the new baseURL prop
+    external: boolean;
   }
-*/
 
-function RecipePreview(props: Recipe) {
-    return (
-          // replace everything between the <div> & </div> tags
-          // with your code from earlier milestones
-
-      <div className="recipe-home">
-        <Link to ={'recipe/${props.name'}>
-        <h2 className="recipe-header">{props.name}</h2></Link>
-        <div className="recipe">
-          <img
-          src={props.image}
-          className="food-preview"
-          alt={props.altDesc}  />
-          <p className="card-desc-home">{props.description}</p>
-        </div>
-        </div>
-    );
+function RecipePreview(props: RecipePreviewProps) {
+  let address = "";
+  if (props.external) {
+    address = "externalRecipe/" + props.name;
+  } else {
+    address = "recipe/" + props.name; // whatever it was before
   }
+
+const id = props.name
+
+return(
+  <div className="recipe-home">
+
+  <Link to={address}> <h2 className="recipe-header">{props.name}</h2>
+  </Link>
+
+  <div className="recipe">
+    <img
+        src={props.image}
+        className="food-preview"
+        alt={props.altDesc}
+     />
+        <p className="card-desc-home">{props.description}</p>
+  </div>
+
+  </div>
+
+)
+
+}
 export default RecipePreview;
