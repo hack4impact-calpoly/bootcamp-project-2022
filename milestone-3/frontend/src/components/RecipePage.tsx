e3387c7e11578a85db7de9cf9a628d04092a5a1f
lemezze <86095301+lemezze@users.noreply.github.com>
2022-12-30 18:23:02 -0800

    feat: making RecipePage interactive

--- milestone-3/frontend/src/components/RecipePage.tsx
+++ milestone-3/frontend/src/components/RecipePage.tsx
@@ -1,66 +1,116 @@
-import React from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import { useParams } from "react-router-dom";
-import recipes from "../recipeData";
+import recipes, { Recipe } from "../recipeData";
 import "./RecipePage.css";
 
-interface RecipePage{
-    name: string;
-    description: string;
-    image: string;
-    ingredients: string[];
-    instructions: string[];
-    altDesc: string
+interface RecipePageProps {
+    external?: boolean;
   }
 
-function RecipePage (){
+  RecipePage.defaultProps = {
+    external: false
+  };
+
+  export default function RecipePage(props: RecipePageProps){
 
     const {id} = useParams();
-    const wanted = recipes.find(recipe => recipe.name === id)
+    const target = recipes.find(recipe => recipe.name == id);
 
-    if (wanted){
+    const [recipe, setRecipe] = useState<Recipe>({
+      name: "",
+      description: "",
+      image: "",
+      ingredients: [],
+      instructions: [],
+      altDesc: ""
+    });
 
-        return(
-            <body>
-            <div className = "all-page">
-                <h1 className="page-recipe-header main-header">{wanted.name}
-                </h1>
-                <div className="recipe-page-first">
-                <img 
-                    src= {wanted.image}
-                    alt= {wanted.altDesc}
-                    className="recipe-img"
-                    />
-                <div className="recipe-desc-ingr">
-                <p className="page-card-desc">{wanted.description}</p>
+    const [newIngredient, setNewIngredient] = useState('');
+    const [allIngredients, setAllIngredients] = useState(recipe.ingredients);
+    const [newInstructions, setNewInstructions] = useState('');
+    const [allInstructions, setAllInstructions] = useState(recipe.instructions);
 
-                <div className="ingredients">
-                <div className = "body-ingredients">
-                  <h3 className = "page-recipe-header main-header">ingredients</h3>
-                        <ul>
-                            {wanted.ingredients.map(function(name, index) {
-                                return <li key={index}>{name}</li>; 
-                            })}
-                        </ul>
-                </div>
-                </div>
-                </div>
-            </div>
+    useEffect(() => {
+      if (props.external) {
+              fetch("https://bootcamp-milestone-4.onrender.com/recipe/" + id)
+              .then((res) => res.json())
+              .then((data) => {setRecipe(data[0])
+              setAllIngredients(data[0].ingredients)
+              setAllInstructions(data[0].instructions)
+              });  
 
-
-            <div className="instructions">
-                  <h3 className="recipe-header-page main-header">instructions</h3>
-                         <ol>
-                            {wanted.instructions.map(function(name, index) {
-                                return <li key={index}> {name} </li> ;
-                            })}
-                        </ol>
- 
-              </div>
-              </div> 
-              </body>
-        )} else {
-            return( <h3>This recipe doesn't exist.</h3> );
+      } else {
+          setRecipe(recipes.find((recipe) => recipe.name === id) || recipes[0])
+          setRecipe(recipes.find((recipe) => recipe.name === id) || recipes[0])
         }
-};
+      }, [id, props.external]);
 
-export default RecipePage;
+      useEffect(() => {
+        setAllIngredients(recipe.ingredients);
+        setAllInstructions(recipe.instructions);
+      }, [recipe]);
+
+  return(
+
+    <body>
+    <div className = "all-page">
+        <h1 className="page-recipe-header main-header">{recipe.name}
+        </h1>
+        <div className="recipe-page-first">
+        <img 
+            src= {recipe.image}
+            alt= {recipe.altDesc}
+            className="recipe-img"
+            />
+        <div className="recipe-desc-ingr">
+        <p className="page-card-desc">{recipe.description}</p>
+
+        <div className="ingredients">
+        <div className = "body-ingredients">
+          <h3 className = "page-recipe-header main-header">Ingredients</h3>
+                <ul>
+                    {allIngredients.map(function(name, index) {
+                      return <li key = {index} > {name}</li>;
+                    })}
+                </ul>
+        <div>
+              <input
+                  placeholder="2 cups of spinach"
+                  value={newIngredient} // add newIngredient as the input's value
+                  onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                    // this event handler updates the value of newIngredient
+                    setNewIngredient(e.target.value);
+                  }}
+                />
+
+              <button onClick ={() => setAllIngredients([...allIngredients, newIngredient])}>
+              Add Ingredient
+              </button>
+          </div>
+          </div>
+          </div>
+          </div>
+        <div className="instructions">
+                  <h3 className="recipe-header-page main-header">instructions</h3>
+                      <ol>
+                        {allInstructions.map(function(name, index) {
+                                return <li key={index} > {name} </li> ;
+                          })}
+                      </ol>
+              <input
+                  placeholder= "Sauté for 30 seconds"
+                  value={newInstructions} // add newInstructions as the input's value
+                  onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                  // this event handler updates the value of newInstructions
+                                    setNewInstructions(e.target.value);
+                                }}
+                            />
+                            <button onClick={() => setAllInstructions([...allInstructions, newInstructions])}>
+                                Add Instruction
+                            </button>
+          </div>
+          </div>
+          </div>
+      </body>
+  )
+};
