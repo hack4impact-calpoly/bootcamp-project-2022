9ce47d02cfac6f8645ef5f3928d825dccb8880b7
Leticia Mezzetti <letimezzetti@gmail.com>
2023-01-06 23:59:09 -0800

    feat: more connecting

--- milestone-3/frontend/src/components/RecipePreview.tsx
+++ milestone-3/frontend/src/components/RecipePreview.tsx
@@ -3,41 +3,41 @@ import "./recipePreview.css"
 import { Link } from "react-router-dom";
 import {Recipe} from "../recipeData";
 
-
+/*
 interface RecipePreviewProps extends Recipe {
   // The extends keyword means that the props include everything
   // from Recipe, as well as the new baseURL prop
     external: boolean;
   }
+*/
 
-function RecipePreview(props: RecipePreviewProps) {
-  let address = "";
+function RecipePreview(props: Recipe) {
+/*  let address = "";
   if (props.external) {
     address = "externalRecipe/" + props.name;
   } else {
     address = "recipe/" + props.name; // whatever it was before
   }
+*/
 
 const id = props.name
 
 return(
-  <div className="recipe-home">
+    <div className="recipe-home">
 
-  <Link to={address}> <h2 className="recipe-header">{props.name}</h2>
-  </Link>
+    <Link to={address}> <h2 className="recipe-header">{props.name}</h2>
+    </Link>
 
-  <div className="recipe">
-    <img
-        src={props.image}
-        className="food-preview"
-        alt={props.altDesc}
-     />
-        <p className="card-desc-home">{props.description}</p>
-  </div>
-
-  </div>
-
-)
+    <div className="recipe">
+      <img
+          src={props.image}
+          className="food-preview"
+          alt={props.altDesc}
+      />
+          <p className="card-desc-home">{props.description}</p>
+    </div>
+    </div>
+  )
 
 }
 export default RecipePreview;
