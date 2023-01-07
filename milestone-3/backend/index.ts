32eb1ed7102d773bbaab17abce5ba2776b6dd76e
Leticia Mezzetti <letimezzetti@gmail.com>
2023-01-06 21:59:09 -0800

    feat: continuing to connect endpoints

--- milestone-3/backend/index.ts
+++ milestone-3/backend/index.ts
@@ -1,4 +1,6 @@
 import { Express } from "express";
+import recipeRoutes from "./routes/recipeRoutes";
+
 const express = require("express"); // 1. includes Express
 const app: Express = express(); // 2. initializes Express
 app.use(express.json()); //allows us to interpret the body of any request
@@ -9,6 +11,13 @@ const connection_url = "mongodb+srv://newUser:elpasswordo@recipescluster.lhzoevj
 mongoose.connect(connection_url)
 .then(()=> console.log("Successfully connected"))
 .catch((error) => console.error(`Could not connect due to ${error}`))
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,DELETE,PUT');
+  next();
+});
 
 // listens for a GET request to be made to the / path.
 // It then executes a function that takes in a request and a response parameter
